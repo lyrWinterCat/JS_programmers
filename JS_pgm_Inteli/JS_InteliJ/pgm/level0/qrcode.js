5b55aec085ad15ada29f38455347b2e019c1516e
lyrWinterCat <dldulfla27@gmail.com>
2024-02-01 11:52:46 +0900

    commit : qrcode 정답

---
+++ JS_pgm_Inteli/JS_InteliJ/pgm/level0/qrcode.js
@@ -0,0 +1,17 @@
+/*
+두 정수 q, r과 문자열 code가 주어질 때,
+code의 각 인덱스를 q로 나누었을 때 나머지가 r인 위치의 문자를
+앞에서부터 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+ */
+
+function solution(q, r, code) {
+    var answer = '';
+    for (let i = 0; i < code.length; i++) {
+        if(i%q===r){
+            answer+=code.charAt(i);
+        }
+    }
+    return answer;
+}
+
+console.log(solution(3,1,"qjnwezgrpirldywt"))
